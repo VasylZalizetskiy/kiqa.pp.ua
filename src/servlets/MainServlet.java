d41175970bcec38b9488deabdbb9e64395475da9
vazyl <zwww@i.ua>
2015-09-24 08:59:30 +0300

    fixed css connection

--- src/servlets/MainServlet.java
+++ src/servlets/MainServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainServlet
  */
-@WebServlet("/")
+@WebServlet("")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
