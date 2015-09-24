f7695fb1d820df0ea59482095244ab86336383d1
vazyl <zwww@i.ua>
2015-09-24 06:12:30 +0300

    temporary project

---
+++ src/servlets/MainServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// no user session found - show login/register page
+        RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
+        view.forward(request, response);
+	}
+
+}
